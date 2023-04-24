83c912dda0ddd4253749d9bc27a37c8b98ac2f6a
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-04-24 15:27:47 +0300

    Added BookListSerializer & BookDetailSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,11 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover_choice", "inventory", "daily_fee")
+
+
+class BookListSerializer(BookSerializer):
+    pass
+
+
+class BookDetailSerializer(BookSerializer):
+    pass
