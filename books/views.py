7d9458c0de4ff4ee45fdde6f242770fba2c02781
usrofgh <mailforgimail@gmail.com>
2023-04-24 16:39:37 +0300

    Add BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import (
+    BookSerializer,
+    BookListSerializer,
+    BookDetailSerializer
+)
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        return self.serializer_class
--- library_config/settings.py
+++ library_config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "drf_spectacular",
     "rest_framework",
-    "user"
+    "user",
+    "books"
 ]
 
 MIDDLEWARE = [
