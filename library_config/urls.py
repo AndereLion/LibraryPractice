7194555a39452a25aa46cbc0a6f94fa5b64e5685
usrofgh <mailforgimail@gmail.com>
2023-04-24 16:36:35 +0300

    Add swagger documentation

--- library_config/urls.py
+++ library_config/urls.py
@@ -4,6 +4,12 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+      ),
     path("admin/", admin.site.urls),
 ] + static(
     settings.MEDIA_URL,
