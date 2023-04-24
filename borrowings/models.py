dc771ad7e9ff3da0ed548f074dc1a3a96fa3bffb
Roman Senyshyn <senjaskorp@gmail.com>
2023-04-24 22:05:59 +0300

    Updated migrations

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,6 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from Book.models import Book
-from User.models import User
+from books.models import Book
 
 
 class Borrowing(models.Model):
@@ -8,4 +8,4 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
