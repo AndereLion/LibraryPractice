aa9c712898de94869ff29ea3345a5b91a6de1b51
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-24 15:20:48 +0300

    add filter/permission functionality for Borrowing list and detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from LibraryPractice.borrowings.models import Borrowing
 from LibraryPractice.borrowings.serializers import (
@@ -8,10 +9,29 @@ from LibraryPractice.borrowings.serializers import (
 
 
 class BorrowingList(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                return Borrowing.objects.select_related("book", "user").filter(user_id=user_id)
+            return Borrowing.objects.select_related("book", "user").all()
+        return Borrowing.objects.select_related("book", "user").filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class BorrowingDetail(generics.RetrieveUpdateAPIView):
-    queryset = Borrowing.objects.all()
     serializer_class = BorrowingDetailSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Borrowing.objects.select_related("book", "user")
+        else:
+            return Borrowing.objects.select_related(
+                "book", "user"
+            ).filter(user=self.request.user)
