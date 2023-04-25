4e09a926a5ad2701179f856114f29454a8d9aa65
oithegod <serejathegod@gmail.com>
2023-04-25 21:38:28 +0300

    added finish for script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,4 @@ python start_bot.py > ./logs/bot.logs 2>&1 &
 celery -A library_config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler > ./logs/celery_beat.logs 2>&1 &
 celery -A library_config worker -l INFO > ./logs/celery_worker.logs 2>&1 &
 python manage.py runserver 0.0.0.0:8000
+done
