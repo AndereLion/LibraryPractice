4a07f27232e50ba945ed997b7d437f8d46498e13
usrofgh <mailforgimail@gmail.com>
2023-04-25 17:44:11 +0300

    Fix urls conflicts

--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
 from payment import views
 
 urlpatterns = [
@@ -12,13 +11,6 @@ urlpatterns = [
     path('success/', views.SuccessView.as_view()),
     path('cancelled/', views.CancelledView.as_view()),
     path('webhook/', views.stripe_webhook),
-=======
-from .views import PaymentListAPIView, PaymentDetailAPIView
-
-urlpatterns = [
-    path('payments/', PaymentListAPIView.as_view(), name='payment_list'),
-    path('payments/<int:pk>/', PaymentDetailAPIView.as_view(), name='payment_detail'),
->>>>>>> 2ff7d1d420c81fa51c21540c2a163ae98a44021e
 ]
 
 app_name = "payment"
