dff6842e491f7868175ad930558505c4f1a49f73
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-26 09:38:51 +0300

    try refactor with only backends for payments, add admin model payment

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "status",
+        "type",
+        "borrowing_id",
+        "money_to_pay"
+    ]
--- payment/views.py
+++ payment/views.py
@@ -5,85 +5,99 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Payment
 from .serializers import PaymentListSerializer, PaymentDetailSerializer
 
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-
-class PaymentListAPIView(generics.ListAPIView):
-    serializer_class = PaymentListSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if self.request.user.is_staff:
-            return Payment.objects.all()
-        else:
-            return Payment.objects.filter(user=self.request.user)
-
-
-class PaymentDetailAPIView(generics.RetrieveAPIView):
-    serializer_class = PaymentDetailSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if self.request.user.is_staff:
-            return Payment.objects.all()
-        else:
-            return Payment.objects.filter(user=self.request.user)
-
-
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+#
+# class PaymentListAPIView(generics.ListAPIView):
+#     serializer_class = PaymentListSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def get_queryset(self):
+#         if self.request.user.is_staff:
+#             return Payment.objects.all()
+#         else:
+#             return Payment.objects.filter(user=self.request.user)
+#
+#
+# class PaymentDetailAPIView(generics.RetrieveAPIView):
+#     serializer_class = PaymentDetailSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def get_queryset(self):
+#         if self.request.user.is_staff:
+#             return Payment.objects.all()
+#         else:
+#             return Payment.objects.filter(user=self.request.user)
 
 
-@csrf_exempt
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def payment_list(request):
+    if request.user.is_staff:
+        payments = Payment.objects.all()
+    else:
+        payments = Payment.objects.filter(user=request.user)
+    serializer = PaymentListSerializer(payments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def payment_detail(request, pk):
+    if request.user.is_staff:
+        payment = Payment.objects.get(pk=pk)
+    else:
+        payment = Payment.objects.get(pk=pk, user=request.user)
+    serializer = PaymentDetailSerializer(payment)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
 def stripe_config(request):
-    if request.method == 'GET':
-        stripe_config = {'publicKey': os.environ.get("STRIPE_PUBLISHABLE_KEY")}
-        return JsonResponse(stripe_config, safe=False)
+    stripe_config = {"publicKey": os.environ.get("STRIPE_PUBLISHABLE_KEY")}
+    return Response(stripe_config)
 
 
-@csrf_exempt
+@api_view(["GET"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def create_checkout_session(request):
-    if request.method == 'GET':
-        domain_url = 'http://localhost:8000/'
-        stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
-        try:
-            checkout_session = stripe.checkout.Session.create(
-                client_reference_id=request.user.id if request.user.is_authenticated else None,
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
-                payment_method_types=['card'],
-                mode='payment',
-                line_items=[
-                    {
-                        "price": os.environ.get("PRICE_KEY"),
-                        'quantity': 1,
-                    }
-                ]
-            )
-            return JsonResponse({'sessionId': checkout_session['id']})
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
+    domain_url = "http://localhost:8000/"
+    stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            client_reference_id=request.user.id if request.user.is_authenticated else None,
+            success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=domain_url + "cancelled/",
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[
+                {
+                    "price": os.environ.get("PRICE_KEY"),
+                    "quantity": 1,
+                }
+            ]
+        )
+        return JsonResponse({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return JsonResponse({"error": str(e)})
 
 
-class SuccessView(TemplateView):
-    template_name = 'success.html'
-
-
-class CancelledView(TemplateView):
-    template_name = 'cancelled.html'
-
-
-@csrf_exempt
+@api_view(["POST"])
 def stripe_webhook(request):
     stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
     endpoint_secret = os.environ.get("STRIPE_ENDPOINT_SECRET")
     payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
     event = None
 
     try:
@@ -98,8 +112,15 @@ def stripe_webhook(request):
         return HttpResponse(status=400)
 
     # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        print("Payment was successful.")
-        # TODO: run some custom code here
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        client_reference_id = session.get("client_reference_id")
+        if client_reference_id:
+            try:
+                payment = Payment.objects.get(pk=client_reference_id)
+                payment.status = "paid"
+                payment.save()
+            except Payment.DoesNotExist:
+                pass
 
     return HttpResponse(status=200)
